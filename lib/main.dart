7083cc360082094b13d362cbbba60655867c9381
Christopher Stone <chrisrunner542@gmail.com>
2024-04-16 18:52:45 -0600

    default layout if setup firebase / also if page size changes then web layout vs phone layout

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram/firebase_options.dart';
+import 'package:instagram/responsive/mobile_screen_layout.dart';
+import 'package:instagram/responsive/responsive_layout_screen.dart';
+import 'package:instagram/responsive/web_screen_layout.dart';
+import 'package:instagram/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    super.key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          //web screen layout
+          return webScreenLayout;
+        }
+        // mobile screen layout
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
