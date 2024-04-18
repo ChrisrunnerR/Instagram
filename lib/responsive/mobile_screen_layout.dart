2559fc0ee07ca9f40bb8a31673d1708cadbc56fc
Christopher Stone <chrisrunner542@gmail.com>
2024-04-18 12:50:09 -0600

    added upload button in bottomnav upload page :)

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,13 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, sort_child_properties_last
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/resources/auth_method.dart';
+import 'package:instagram/screens/add_post_screen.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/global_variables.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram/models/user.dart' as model;
 
@@ -17,11 +21,19 @@ class MobileScreenLayout extends StatefulWidget {
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   String username = "";
   String email = "";
+  late PageController pageController;
 
   @override
   void initState() {
     super.initState();
     getUsername();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
   }
 
 // should just be void
@@ -36,20 +48,63 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
     }
   }
 
+  int _page = 0;
+
+  void NavigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page"), actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.exit_to_app),
-          onPressed: () {
-            AuthMethods().signOut();
-          },
-          tooltip: "Logout",
-        )
-      ]),
-      body: Center(
-        child: Text(username, style: TextStyle(color: Colors.white)),
+      body: PageView(
+        //in global variables
+        children: homeScreenItems,
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor),
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor),
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor),
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor),
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor),
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: NavigationTapped,
       ),
     );
     ;
---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: IconButton(onPressed: () {}, icon: Icon(Icons.upload)),
+    );
+  }
+}
