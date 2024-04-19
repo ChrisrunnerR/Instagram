1e7e4b7ebbfa0e447d3649e2013068e8f9046313
Christopher Stone <chrisrunner542@gmail.com>
2024-04-18 18:29:49 -0600

    upload works now uploads photos to firebase store grabs download link and store in firestore as well.

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,36 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> uploadPost(String description, Uint8List file, String uid,
+      String username, String profImage) async {
+    String result = "Some error occured";
+    try {
+      if (description.isNotEmpty || file != null || uid.isNotEmpty) {
+        String photoUrl =
+            await StorageMethods().uploadImageToStorage('posts', file, true);
+        String postId = const Uuid().v1();
+
+        await _firestore.collection('posts').doc(postId).set({
+          'description': description,
+          'username': username,
+          'uid': uid,
+          'postId': postId,
+          'datePublished': DateTime.now(),
+          'postUrl': photoUrl,
+          'profImage': profImage,
+        });
+        result = "success";
+      }
+    } catch (err) {
+      result = err.toString();
+    }
+    print(result);
+    return result;
+  }
+}
--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,6 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram/resources/auth_method.dart';
+import 'package:instagram/resources/firestore_methods.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -10,10 +16,176 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  String _profImage = "";
+  String _username = "";
+  String _uid = "";
+  final TextEditingController _descriptionController = TextEditingController();
+
+  void postImage(
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    print("uid : ${uid},  profile_Image ${_profImage}");
+
+    try {
+      String res = await FirestoreMethods().uploadPost(
+          _descriptionController.text, _file!, uid, username, profImage);
+      if (res == "success") {
+        showSnackBar('Posted!', context);
+      } else {
+        showSnackBar(res, context);
+      }
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDetails();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
+// should just be void
+  void getUserDetails() async {
+    var userDetails = await AuthMethods().getUserDetails();
+
+    if (userDetails != null) {
+      setState(() {
+        _profImage = userDetails['photoUrl'];
+        _username = userDetails['username'];
+        _uid = userDetails['uid'];
+      });
+    }
+  }
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text("Create a Post"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Take a photo"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Choose a photo"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(onPressed: () {}, icon: Icon(Icons.upload)),
-    );
+    return _file == null
+        ? Center(
+            child: IconButton(
+                onPressed: () => _selectImage(context),
+                icon: Icon(Icons.upload)),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back),
+              ),
+              title: const Text('Post to'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () => {
+                    postImage(
+                      _uid,
+                      _username,
+                      _profImage,
+                    )
+                  },
+                  child: Text(
+                    'Post',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            body: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(_profImage),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: InputDecoration(
+                          hintText: "Write a caption...",
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: MemoryImage(_file!),
+                              fit: BoxFit.fill,
+                              alignment: FractionalOffset.topCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Divider(),
+                  ],
+                )
+              ],
+            ),
+          );
   }
 }
